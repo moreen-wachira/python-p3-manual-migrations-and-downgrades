17c0b9ae853a5dca706b5400dcdcb93aec70c757
Moreen <moreencharles@gmail.com>
2024-01-11 20:16:35 +0300

    Completed Assignment

--- lib/models.py
+++ lib/models.py
@@ -1,15 +1,14 @@
+# Assuming your Student model looks like this
 import os
 import sys
 
 sys.path.append(os.getcwd())
 
-
 from datetime import datetime
-
 from sqlalchemy import create_engine, desc
-from sqlalchemy import (CheckConstraint, UniqueConstraint,
-    Column, DateTime, Integer, String)
-
+from sqlalchemy import (
+    CheckConstraint, UniqueConstraint, Column, DateTime, Integer, String
+)
 from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine('sqlite:///migrations_test.db')
@@ -20,7 +19,7 @@ class Student(Base):
     __tablename__ = 'students'
 
     id = Column(Integer(), primary_key=True)
-    name = Column(String(), index=True)
+    new_name = Column(String(255))  # Rename 'new_column_name' to 'new_name'
     email = Column(String(55))
     grade = Column(Integer())
     birthday = Column(DateTime())
@@ -28,5 +27,5 @@ class Student(Base):
 
     def __repr__(self):
         return f"Student {self.id}: " \
-            + f"{self.name}, " \
+            + f"{self.new_name}, " \
             + f"Grade {self.grade}"
